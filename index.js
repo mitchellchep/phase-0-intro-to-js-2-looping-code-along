0dc8803ba94900ff5a8ffbffabf871241d972001
mitchellkoech <mitchikoechi3.7@gmail.com>
2023-09-23 22:57:52 +0300

    Completed assignment
    Signed-off-by: mitchellkoech <mitchikoechi3.7@gmail.com>

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names, action){
+    let replies = []
+    for (let i = 0; i < names.length; i++){
+        let reply = `Thank you, ${names[i]}, for the wonderful ${action} gift!`;
+        replies.push(reply);
+    }
+    return replies
+}
+
+function countDown(n){
+    while(n >= 0){
+        console.log(n);
+        n--;
+    }
+}
